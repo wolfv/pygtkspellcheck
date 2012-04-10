08f89dbd9eb320869de292a30a5b875b856a23b7
Maximilian Köhl <linuxmaxi@googlemail.com>
2012-04-10 19:55:26 +0200

    documentation update

--- src/setup.py
+++
@@ -1,5 +0,0 @@
-from distutils.core import setup
-setup(name='pygtkspellchecker',
-      version='1.0',
-      packages=['gtkspellcheck', 'pylocale'],
-      package_data={'pylocale' : ['locales.db', 'locales/*/*/*']})
